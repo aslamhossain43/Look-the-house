76d9cebcb9409489d90773e5ef90d183015470e8
Atif Aslam <aslam.renu.soft43@gmail.com>
2018-09-07 02:58:47 -0800

    Update has been completed successfully

---
+++ Look_the_house/src/main/java/com/renu/look/house/repository/AddServiceRepository.java
@@ -0,0 +1,32 @@
+package com.renu.look.house.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.renu.to_let.models.AddService;
+
+public interface AddServiceRepository extends JpaRepository<AddService, Long>,JpaSpecificationExecutor<AddService>{
+	//get by name
+	public AddService findByWord(String word);
+	//get all services
+	@Query("FROM AddService")
+	public List<AddService>getServicesTable();
+	//find by category
+	public static final String category="FROM AddService where rentType=:category";
+        	@Query(category)
+        List<AddService> findByCategory(@Param("category")String category);
+        	//find by category
+        	public static final String country="FROM AddService where country=:country";
+                	@Query(country)
+			public List<AddService> findByCountry(@Param("country")String country);
+                	public static final String countryCategory="FROM AddService where rentType=:category AND country=:country";
+                	@Query(countryCategory)
+					public List<AddService> findByCountryCategory(@Param("category")String category,@Param("country")String country);
+                	
+            
+        
+}
