dbf4070b3b3ba912cceff2a8eec8bd0b51c8ca02
Atif Aslam <aslam.renu.soft43@gmail.com>
2018-09-12 17:03:29 -0800

    Update has been completed successfully

--- Look_the_house/src/main/java/com/renu/look/house/controller/AuthenticationController.java
+++ Look_the_house/src/main/java/com/renu/look/house/controller/AuthenticationController.java
@@ -1,19 +1,26 @@
 package com.renu.look.house.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.renu.look.house.models.User;
+import com.renu.look.house.repository.User_Repository;
+
 @ControllerAdvice
 public class AuthenticationController {
-
+ @Autowired
+ private User_Repository user_Repository;
 	
-	@ModelAttribute("authorityname")
-	public String getEmail() {
+	@ModelAttribute("authorityObject")
+	public User getEmail() {
 		Authentication authentication=SecurityContextHolder.getContext().getAuthentication();
 		String email=authentication.getName();
-		return email;
+		User user=user_Repository.getByUsername(email);
+		
+		return user;
 		
 	}
 	
