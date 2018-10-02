87eeb582fe79b3749c3ed835148da74dbaff61b4
Atif Aslam <aslam.renu.soft43@gmail.com>
2018-10-01 16:19:17 -0800

    Update completed successfully

--- Look_the_house/src/main/java/com/renu/look/house/web/repository/UserRepository.java
+++ Look_the_house/src/main/java/com/renu/look/house/web/repository/UserRepository.java
@@ -1,10 +1,21 @@
 package com.renu.look.house.web.repository;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.renu.look.house.models.User;
 
 
 public interface UserRepository extends JpaRepository<User, Long>{
   public User findByUsername(String username);
+  @Modifying
+	@Transactional
+ @Query("UPDATE User SET password=:pwd WHERE username=:username")
+	public void updatePassword(@Param("pwd") String pwd, @Param("username")String username);
+	
+  
 }
